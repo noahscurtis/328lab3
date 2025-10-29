b23a40477b456c18233db5894f2191b3dfb9701d
noahscurtis <noahscurtis@gmail.com>
2025-10-29 15:41:15 -0700

    earthquake map

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiamFrb2J6aGFvIiwiYSI6ImNpcms2YWsyMzAwMmtmbG5icTFxZ3ZkdncifQ.P9MBej1xacybKcDN_jehvw';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibmN1cnRpczQiLCJhIjoiY21oY2syc2FyMWJkbTJrcHpvbHVpbTh3NiJ9.IkITHi8RWT9S7GeJKh-QUQ';
 
 let map = new mapboxgl.Map({
     container: 'map', // container ID
@@ -13,58 +14,97 @@ async function geojsonFetch() {
     earthquakes = await response.json();
     response = await fetch('assets/japan.json');
     japan = await response.json();
+    //load data to the map as new layers and table on the side.
+    map.on('load', function loadingData() {
+
+        map.addSource('earthquakes', {
+            type: 'geojson',
+            data: earthquakes
+        });
+
+        map.addLayer({
+            'id': 'earthquakes-layer',
+            'type': 'circle',
+            'source': 'earthquakes',
+            'paint': {
+                'circle-radius': 8,
+                'circle-stroke-width': 2,
+                'circle-color': 'red',
+                'circle-stroke-color': 'white'
+            }
+        });
+
+
+        map.addSource('japan', {
+            type: 'geojson',
+            data: japan
+        });
+
+        map.addLayer({
+            'id': 'japan-layer',
+            'type': 'fill',
+            'source': 'japan',
+            'paint': {
+                'fill-color': '#0080ff', // blue color fill
+                'fill-opacity': 0.5
+            }
+        });
+
+    });
+
+    table = document.getElementsByTagName("table")[0];
+    let row, cell1, cell2, cell3;
+    for (let i = 0; i < earthquakes.features.length; i++) {
+        // Create an empty <tr> element and add it to the 1st position of the table:
+        row = table.insertRow(-1);
+        cell1 = row.insertCell(0);
+        cell2 = row.insertCell(1);
+        cell3 = row.insertCell(2);
+        cell1.innerHTML = earthquakes.features[i].properties.id;
+        cell2.innerHTML = earthquakes.features[i].properties.mag;
+        cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
+            "en-US");
+    }
 };
 
 geojsonFetch();
 
-//load data to the map as new layers and table on the side.
-map.on('load', function loadingData() {
+let btn = document.getElementsByTagName("button")[0];
+btn.addEventListener('click', sortTable);
 
-    map.addSource('earthquakes', {
-        type: 'geojson',
-        data: earthquakes
-    });
-
-    map.addLayer({
-        'id': 'earthquakes-layer',
-        'type': 'circle',
-        'source': 'earthquakes',
-        'paint': {
-            'circle-radius': 8,
-            'circle-stroke-width': 2,
-            'circle-color': 'red',
-            'circle-stroke-color': 'white'
+// define the function to sort table
+function sortTable(e) {
+    let table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementsByTagName("table")[0];
+    switching = true;
+    /*Make a loop that will continue until
+    no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /*Loop through all table rows (except the
+        first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = parseFloat(rows[i].getElementsByTagName("td")[1].innerHTML);
+            y = parseFloat(rows[i + 1].getElementsByTagName("td")[1].innerHTML);
+            //check if the two rows should switch place:
+            if (x < y) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+                break;
+            }
         }
-    });
-
-
-    map.addSource('japan', {
-        type: 'geojson',
-        data: japan
-    });
-
-    map.addLayer({
-        'id': 'japan-layer',
-        'type': 'fill',
-        'source': 'japan',
-        'paint': {
-            'fill-color': '#0080ff', // blue color fill
-            'fill-opacity': 0.5
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
         }
-    });
-
-});
-
-table = document.getElementsByTagName("table")[0];
-let row, cell1, cell2, cell3;
-for (let i = 0; i < earthquakes.features.length; i++) {
-    // Create an empty <tr> element and add it to the 1st position of the table:
-    row = table.insertRow(-1);
-    cell1 = row.insertCell(0);
-    cell2 = row.insertCell(1);
-    cell3 = row.insertCell(2);
-    cell1.innerHTML = earthquakes.features[i].properties.id;
-    cell2.innerHTML = earthquakes.features[i].properties.mag;
-    cell3.innerHTML = new Date(earthquakes.features[i].properties.time).toLocaleDateString(
-        "en-US");
+    }
 }
+
